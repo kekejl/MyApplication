7ab7eb963b307f0394572bf36a42aafe66399fad
791745064@qq.com <zxcvbnm123456>
2017-02-22 10:36:29 +0800

    第一次提交

---
+++ app/src/main/java/com/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private String  url="99999";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
